489c7525e002ee2dd737c647cef066dae52042f4
Valentin Gillot <valentingillot@gmail.com>
2026-02-19 12:08:40 +0100

    I'm batman

---
+++ go.mod
@@ -0,0 +1,9 @@
+module make-your-own
+
+go 1.25.6
+
+require (
+	github.com/boltdb/bolt v1.3.1 // indirect
+	go.etcd.io/bbolt v1.4.3 // indirect
+	golang.org/x/sys v0.29.0 // indirect
+)
---
+++ transaction.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"bytes"
+	"crypto/sha256"
+	"encoding/gob"
+	"encoding/hex"
+	"fmt"
+	"log"
+)
+
+type Transaction struct {
+	ID   []byte
+	Vin  []TXInput
+	Vout []TXOutput
+}
+
+// In Bitcoin, the value field stores the number of satoshis, not the number of BTC.
+// A satoshi is a hundred millionth of a bitcoin (0.00000001 BTC)
+// this is the smallest unit of currency in Bitcoin (like a cent).
+type TXOutput struct {
+	Value        int
+	ScriptPubKey string
+}
+
+type TXInput struct {
+	Txid      []byte
+	Vout      int
+	ScriptSig string
+}
+
+// NewCoinbaseTX creates a new coinbase transaction
+func NewCoinbaseTX(to, data string) *Transaction {
+	if data == "" {
+		data = fmt.Sprintf("Reward to '%s'", to)
+	}
+	txin := TXInput{[]byte{}, -1, data}
+	txout := TXOutput{subsidy, to}
+	tx := Transaction{nil, []TXInput{txin}, []TXOutput{txout}}
+	tx.SetID()
+
+	return &tx
+}
+
